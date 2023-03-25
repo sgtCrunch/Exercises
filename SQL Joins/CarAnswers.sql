21cd7e3cd18cedf11a154d41d47c7668b01b2457
Brayan Pereira <brayan@codewithus.com>
2023-03-25 11:46:51 -0700

    Added SQL join excercises

---
+++ SQL Joins/CarAnswers.sql
@@ -0,0 +1,33 @@
+
+-- Join the two tables so that every
+-- column and record appears, regardless 
+-- of if there is not an owner_id .
+-- Your output should look like this:
+SELECT * FROM owners LEFT JOIN 
+vehicles ON owners.id = vehicles.owner_id;
+
+-- Count the number of cars for each owner. 
+-- Display the owners first_name , last_name
+--  and count of vehicles. The first_name 
+-- should be ordered in ascending order. 
+
+SELECT first_name, last_name, COUNT(*) 
+FROM owners 
+JOIN vehicles ON owners.id = vehicles.owner_id 
+GROUP BY owners.id ORDER BY first_name;
+
+-- Count the number of cars for each owner and display 
+-- the average price for each of the cars as integers. 
+-- Display the owners first_name , last_name, average 
+-- price and count of vehicles. The first_name should 
+-- be ordered in descending order. Only display results 
+-- with more than one vehicle and an average price 
+-- greater than 10000.
+
+SELECT first_name, last_name, ROUND(AVG(price))
+AS average_price, COUNT(*) 
+FROM owners
+JOIN vehicles ON owners.id = vehicles.owner_id  
+GROUP BY owners.id
+HAVING AVG(price) > 10000 AND COUNT(*) > 1
+ORDER BY first_name DESC;
